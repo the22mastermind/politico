0ea836d09ea0d9d5282714c41c5cd0c1eae7a1ef
themastermind <bertrandmasabo22@gmail.com>
2019-02-12 19:21:21 +0200

    heroku deployment fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import router from './routes/routes';
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
